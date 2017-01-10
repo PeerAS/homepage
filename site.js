c3ede2fcecbd4b9f33f3566281f8f62d15015ceb
Peer Andreas Stange <Peer Andreas Stange>
2017-01-10 17:54:43 +0100

    lagt til skuff på about me

--- site.js
+++ site.js
@@ -3,8 +3,40 @@
 
     function HelloWorld(params) {
         console.log("Hello");
+        $('#aboutMe').on('click', function()
+        {
+            if($("#aboutMeContent").height() === 0)
+            {
+                $("#aboutMeContent").animate({
+                    height:"400px",
+                    
+                },
+                {  start: function()
+                    {
+                        $(this).css("display", "flex");
+                    }
+                }, 600);
+            }
+            else
+            {
+                $("#aboutMeContent").animate({
+                    height:"0px"                   
+                },
+                {
+                     done: function()
+                    {
+                        $(this).css("display", "none");
+                    }
+                }, 600);
+            }
+           
+        });
+  
     }
 
+
+  
+           
     return Emyra = {
         HelloWorld: HelloWorld
     }
